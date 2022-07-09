c9c36a362a1746f019bca6fef15a9b0d72d68880
milad <mohammadsamadi1374@gmail.com>
2022-07-09 14:40:13 +0430

    make base pages

--- Desktop/d/src/App.js
+++ Desktop/d/src/App.js
@@ -1,4 +1,12 @@
 import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
+import Profile from "./pages/Profile";
 
 function App() {
   const [login, setLogin] = useState(false);
@@ -11,7 +19,51 @@ function App() {
     setLogin(false);
   };
 
-  return <div className="App">aaa</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={<Home login={login} logoutUser={logoutUser} />}
+          />
+          <Route
+            path="/home"
+            element={<Home login={login} logoutUser={logoutUser} />}
+          />
+          <Route
+            path="/product"
+            element={<Product login={login} logoutUser={logoutUser} />}
+          />
+          <Route
+            path="/login"
+            element={
+              login == true ? (
+                <Navigate to="/home" />
+              ) : (
+                <Login loginUser={loginUser.bind(this)} />
+              )
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              login == true ? (
+                <Profile login={login} logoutUser={logoutUser} />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/cart"
+            element={<Cart login={login} logoutUser={logoutUser} />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
